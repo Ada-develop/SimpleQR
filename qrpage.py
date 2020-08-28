cba09c09d6e0f129040500b76bf498e795684cf9
adamj <adam.jarmolovic@gmail.com>
2020-08-28 22:57:35 +0300

    Added .png file and comments

--- qrpage.py
+++ qrpage.py
@@ -1,9 +1,17 @@
+#Importing tools:
+
 import pyqrcode
 import png
 from pyqrcode import QRCode
 
-QRstring = "http://192.168.1.72/"
+#Initialize string for QR representation
+
+QRstring = "https://github.com/Ada-develop"
+
+#Creating QR of a string
 
 url = pyqrcode.create(QRstring)
+
+#Converting QR to .png format
 
 url.png('./qr.png', scale=8)
